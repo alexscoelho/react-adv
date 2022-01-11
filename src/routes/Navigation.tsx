7845a4ada1db2e0a593cecf014ba4886f2f5c58a
Alexson <alexsoncoelho21@hotmail.com>
2022-01-10 21:24:26 -0500

    Nested Routes

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -35,7 +35,7 @@ export const Navigation = () => {
                 element={<route.Component />}
               />
             ))}
-            <Route path='/*' element={<Navigate to={routes[0].to} replace />} />
+            {/* <Route path='*' element={<Navigate to={routes[0].to} replace />} /> */}
           </Routes>
         </div>
       </BrowserRouter>
