6b12cd80a58169c62a965c60d30a8025967db06a
Alexson <alexsoncoelho21@hotmail.com>
2022-01-19 20:41:07 -0500

    Control Props Alt

--- src/02-component-patterns/hooks/useShoppingCart.ts
+++ src/02-component-patterns/hooks/useShoppingCart.ts
@@ -14,32 +14,15 @@ export const useShoppingCart = () => {
     product: Product;
   }) => {
     setShoppingCart((oldShoppingCart) => {
-      const productInCart: ProductInCart = oldShoppingCart[product.id] || {
-        ...product,
-        count: 0,
-      };
+      if (count === 0) {
+        const { [product.id]: toDelete, ...rest } = oldShoppingCart;
 
-      if (Math.max(productInCart.count + count, 0) > 0) {
-        productInCart.count += count;
-        return {
-          ...oldShoppingCart,
-          [product.id]: productInCart,
-        };
+        return rest;
       }
-
-      const { [product.id]: toDelete, ...rest } = oldShoppingCart;
-      return rest;
-
-      // Delete product that has quantity 0 - uncontrolled implementation...
-      // if (count === 0) {
-      //   const { [product.id]: toDelete, ...rest } = oldShoppingCart;
-
-      //   return rest;
-      // }
-      // return {
-      //   ...oldShoppingCart,
-      //   [product.id]: { ...product, count },
-      // };
+      return {
+        ...oldShoppingCart,
+        [product.id]: { ...product, count },
+      };
     });
   };
 
